7be141bab3ef982631844ed4a78cf0df0e4eb9c1
bo.wang <bo.wang@clickzetta.com>
2025-07-14 14:09:06 +0800

    optimize bash output

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,7 +1,9 @@
+# 在 .zshrc 中添加（替换原有的颜色定义）
 autoload -U colors && colors
-RED=$fg[red]
-GRN=$fg[green]
-BLU=$fg[blue]
-MAG=$fg[magenta]
-RST=$reset_color
 
+# 推荐的颜色定义方式（用于提示符）
+RED="%F{red}"
+GRN="%F{green}"
+BLU="%F{blue}"
+MAG="%F{magenta}"
+RST="%f"  # 重置颜色（不是 $reset_color）
